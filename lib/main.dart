5efeb8f7d4d0633f2b4bb64a30233b71f224c0be
erwinagpasa <istran.net@gmail.com>
2020-12-29 12:40:35 +0300

    text provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_app/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  //For providers to work,
+  //you must add ProviderScope
+  //at the root of your Flutter applications:
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -9,58 +15,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyHomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final textRiverpod = useProvider(textProvider);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Riverpod Demo'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        child: Center(
+          child: Text('yaw!!! ' + textRiverpod),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/provider.dart
+++ lib/provider.dart
@@ -1,3 +1,11 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+//Provider - is the most basic of all providers. It exposes an object
+//that never changes. We could replace Provider with other providers
+//like StreamProvider or StateNotifierProvider, to change how the
+//value is interacted with.
+
+//(ref) - That function will always receive an object called ref as a parameter.
+//This object allows us to read other providers or to perform some
+//operations when the state of our provider will be destroyed.
 final textProvider = Provider((ref) => 'Hello riverpod');
